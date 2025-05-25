11ca99aebc3efd3a5fd1f5f837c0e6f51ad6d235
Joe D'Amore <joe@joedamore.me>
2025-05-25 12:58:51 -0400

    Add Slint hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+slint::include_modules!();
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let screen_width = std::env::var("HELLO_SLINT_WIDTH").unwrap_or("640".to_string());
+    let screen_height = std::env::var("HELLO_SLINT_HEIGHT").unwrap_or("480".to_string());
+
+    let ui = MainWindow::new()?;
+
+    ui.set_screen_width(screen_width.parse::<i32>().unwrap());
+    ui.set_screen_height(screen_height.parse::<i32>().unwrap());
+
+    ui.run()?;
+
+    Ok(())
 }
